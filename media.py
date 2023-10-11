d40aadb562ed1d043107c59092d637fde8ed5efd
janeilson-araujo <janeilsonpaulino013@gmail.com>
2023-10-11 12:36:18 -0300

    atividade01 media

---
+++ media.py
@@ -0,0 +1,7 @@
+nome = str(input("nome do aluno:"))
+materia = str(input("nome da materia:"))
+nota1 = float(input("insira nota 1: "))
+nota2 = float(input("insira nota 2: "))
+nota3 = float(input("insira nota 3: ")) 
+media = (nota1+nota2+nota3)/3
+print(f" a media do aluno {nome} \n na materia {materia} foi:{media}")
